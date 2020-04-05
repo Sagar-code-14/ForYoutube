7e7aa42a9ba8a3e73310e1d194e7bb6e0b38c8f9
sagar <sagar.rganechary@gmail.com>
2020-04-05 17:39:17 +0530

    large files uploaded by me for testing purpose
    Signed-off-by: sagar <sagar.rganechary@gmail.com>

---
+++ public/head.php
@@ -0,0 +1,32 @@
+<?php include_once("functions/pageLabels.php");?>
+<?php include_once("functions/companyInfo.php")?>
+<head>
+    <meta charset="UTF-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
+    
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    
+    <!-- Owl Carousele CSS -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+    
+    <!-- Slicknav CSS -->
+    <link rel="stylesheet" href="assets/css/slicknav.min.css">
+    
+    <!-- Magnific  Popup CSS -->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    
+     <!-- Main CSS -->
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    
+    <!-- Main JS -->
+    <script src="assets/js/jquery-1.11.3.js"></script>
+</head>
